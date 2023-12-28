c7cf34113913bc50ea5eacc166ca7fd813a299cf
Shivek <sharmashivek18@gmail.com>
2023-12-28 14:25:31 +0530

    display chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -22,7 +22,7 @@ class ChirpController extends Controller
         // return response('Hello, World!')
 
         return Inertia::render('Chirps/Index', [
-            //
+            'chirps' => Chirp::with('user:id,name')->latest()->get(),
         ]);
     }
 
--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Chirp extends Model
 {
@@ -17,4 +18,10 @@ class Chirp extends Model
     protected $fillable = [
         'message',
     ];
+
+    // 'belongsTo' is the inverse of 'hasMany' relationship we created in 'User' model
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
